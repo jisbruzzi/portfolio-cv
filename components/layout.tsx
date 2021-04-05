a96850f4259f0e126ac68d2b7050691d1d3ad63f
jisbruzzi <jose.sbru@gmail.com>
2021-04-04 22:34:03 -0300

    some layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,12 +7,17 @@ type Props = {
 
 const Layout = ({ preview, children }: Props) => {
   return (
-    <>
+    <div>
       <Meta />
-      <div className="min-h-screen">
-        <main>{children}</main>
+      
+      <div className="min-h-screen mx-auto max-w-screen-sm mt-12">
+        <h1 className="text-4xl font-black font-mono">Un cuaderno con ideas</h1>
+        <p className="font-black font-mono">El blog de José Ignacio Sbruzzi</p>
+        <main className="pt-12">
+          {children}
+        </main>
       </div>
-    </>
+    </div>
   )
 }
 
