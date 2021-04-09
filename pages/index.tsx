eee27430605486d573580f6c17e20a4134e2fc09
jisbruzzi <jose.sbru@gmail.com>
2021-04-09 14:37:47 -0300

    small fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,10 @@ import type { GrayMatterFile } from "gray-matter"
 import { getBySlug } from "../lib/landingParts"
 import markdownToHtml from "../lib/markdownToHtml"
 
-interface HomeProps{
-  jose:GrayMatterFile<string>
+interface HomeProps {
+  jose: GrayMatterFile<string>
 }
-function PhotoAndInformation({data}:{data:{[key:string]:any}}){
+function PhotoAndInformation({ data }: { data: { [key: string]: any } }) {
   return <div className="
   m-auto
   flex
@@ -14,16 +14,16 @@ function PhotoAndInformation({data}:{data:{[key:string]:any}}){
   items-center
   justify-center
   h-full
+  p-4
   ">
     {
       data.name && <div className="
       text-white
       text-6xl
-      w-2/3
       rounded-full
       font-montserrat
       text-center
-      my-4
+      m-4
       font-bold
       ">
         {data.name}
@@ -31,8 +31,8 @@ function PhotoAndInformation({data}:{data:{[key:string]:any}}){
     }
 
     {
-      (typeof data.photo === "string") && 
-        <img 
+      (typeof data.photo === "string") &&
+      <img
         className="
         rounded-full
         w-2/3
@@ -44,13 +44,12 @@ function PhotoAndInformation({data}:{data:{[key:string]:any}}){
         my-4
         "
         src={data.photo}
-        />
+      />
     }
 
     {
       data.caption && <div className="
       text-white
-      w-2/3
       rounded-full
       font-montserrat
       text-center
@@ -61,23 +60,22 @@ function PhotoAndInformation({data}:{data:{[key:string]:any}}){
         {data.caption}
       </div>
     }
-    
+
   </div>
 }
-function Curriculum({person}:{person:GrayMatterFile<string>}){
+function Curriculum({ person }: { person: GrayMatterFile<string> }) {
   return <div className="flex lg:flex-row flex-col lg:min-h-almost-screen">
     <div className="
     bg-blue-900
     lg:w-1/3
     rounded-xl
     m-8
-    p-4
     shadow-xl
     relative
     ">
-      <PhotoAndInformation data={person.data}/>
+      <PhotoAndInformation data={person.data} />
     </div>
-    <div 
+    <div
       className="
       prose
       m-auto
@@ -86,27 +84,27 @@ function Curriculum({person}:{person:GrayMatterFile<string>}){
     />
   </div>
 }
-export default function Homepage(props:HomeProps){
+export default function Homepage(props: HomeProps) {
   console.log(props)
   return <div className="container mx-auto">
-    <Curriculum person={props.jose}/>
+    <Curriculum person={props.jose} />
   </div>
 }
 
-export async function getStaticProps():Promise<{props:HomeProps}> {
-  async function htmlPart(name:string):Promise<GrayMatterFile<string>>{
+export async function getStaticProps(): Promise<{ props: HomeProps }> {
+  async function htmlPart(name: string): Promise<GrayMatterFile<string>> {
     const post = getBySlug(name)
     const content = await markdownToHtml(post.content || '')
     return {
       ...post,
       content,
-      orig:""
+      orig: ""
     }
   }
 
   return {
     props: {
-      jose:(await htmlPart("jose"))
+      jose: (await htmlPart("jose"))
     },
   }
 }
