29453ac8e564e9275d142a3a6e50443a4a8f0894
jisbruzzi <jose.sbru@gmail.com>
2021-04-10 14:59:36 -0300

    extract portfolioitem

---
+++ components/landing/PortfolioItem.tsx
@@ -0,0 +1,41 @@
+import { GrayMatterFile } from "gray-matter";
+import React from "react";
+import Card from "./Card";
+
+export default function PortfolioItem({ information }: { information: GrayMatterFile<string> }) {
+  return <div className="my-8">
+    <Card>
+      <div className="flex flex-row h-36">
+        {
+          information.data.cover && <>
+            <img
+              src={information.data.cover}
+              className="
+                w-1/3
+                rounded-xl
+                shadow
+                border-blue-100
+                border-solid
+                border-4
+                object-cover
+                "
+            />
+          </>
+        }
+        {
+          information.data.title && <div className="p-4 w-2/3">
+            <h1 className="
+              text-2xl
+              font-montserrat
+              font-bold
+              ">
+              {information.data.title}
+            </h1>
+            <a className="hover:underline text-sm text-blue-900" href={information.data.website}>website</a>
+            <div dangerouslySetInnerHTML={{ __html: information.data.excerpt }} />
+          </div>
+        }
+      </div>
+    </Card>
+  </div>
+}
