1bea4abbb4859d68360809a816419eae25657ef3
jisbruzzi <jose.sbru@gmail.com>
2021-04-10 15:55:05 -0300

    blue background for hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import PortfolioItem from "../components/landing/PortfolioItem"
 import { getBySlug, getPortfolioItems } from "../lib/landingParts"
 import markdownToHtml from "../lib/markdownToHtml"
 
-function SectionTitle(props:PropsWithChildren<{}>){
+function SectionTitle(props: PropsWithChildren<{}>) {
   return <h1 className="
   text-center text-4xl 
   font-montserrat m-4 font-extrabold 
@@ -29,9 +29,9 @@ function DarkBlock({ text }: { text: GrayMatterFile<string> }) {
           </span>
         </SectionTitle>
       }
-      <div 
+      <div
         className="prose prose-white prose-compact max-w-none"
-        dangerouslySetInnerHTML={{ __html: text.content }} 
+        dangerouslySetInnerHTML={{ __html: text.content }}
       />
     </ContentContainer>
   </div>
@@ -44,38 +44,40 @@ interface HomeProps {
 }
 
 function Curriculum({ person }: { person: GrayMatterFile<string> }) {
-  return <div className="container mx-auto">
-  <div className="flex lg:flex-row flex-col align-middle">
-    <div className="
+  return <div className="bg-blue-50">
+    <div className="container mx-auto">
+      <div className="flex lg:flex-row flex-col align-middle">
+        <div className="
     lg:w-1/3
     lg:m-8
     m-4
     ">
-      <div className="
+          <div className="
       lg:min-h-almost-screen
       sticky top-8
       ">
-        <Card dark>
-          <PhotoAndInformation data={person.data} />
-        </Card>
+            <Card dark>
+              <PhotoAndInformation data={person.data} />
+            </Card>
+          </div>
+        </div>
+        <div className="lg:w-2/3 lg:m-8 m-4">
+          <Hero data={person.data} />
+          <div
+            className="prose mx-auto my-8 prose-compact prose-weaker"
+            dangerouslySetInnerHTML={{ __html: person.content }}
+          />
+        </div>
       </div>
     </div>
-    <div className="lg:w-2/3 lg:m-8 m-4">
-      <Hero data={person.data} />
-      <div
-      className="prose mx-auto my-8 prose-compact prose-weaker"
-      dangerouslySetInnerHTML={{ __html: person.content }}
-      />
-    </div>
-  </div>
   </div>
 }
 
-function Portfolio(props:{portfolioItems:GrayMatterFile<string>[]}){
+function Portfolio(props: { portfolioItems: GrayMatterFile<string>[] }) {
   return <div className="bg-blue-50">
     <ContentContainer>
       <SectionTitle> Portfolio </SectionTitle>
-      {props.portfolioItems.map(item=><PortfolioItem information={item}/>)}
+      {props.portfolioItems.map(item => <PortfolioItem information={item} />)}
     </ContentContainer>
   </div>
 }
@@ -83,8 +85,8 @@ export default function Homepage(props: HomeProps) {
   console.log(props)
   return <>
     <Curriculum person={props.jose} />
-    <DarkBlock text={props.experience}/>
-    <Portfolio portfolioItems={props.portfolio}/>
+    <DarkBlock text={props.experience} />
+    <Portfolio portfolioItems={props.portfolio} />
   </>
 }
 
@@ -103,7 +105,7 @@ export async function getStaticProps(): Promise<{ props: HomeProps }> {
     props: {
       jose: (await htmlPart("jose.md")),
       experience: (await htmlPart("experience.md")),
-      portfolio: await Promise.all(getPortfolioItems().map(name=>htmlPart(`portfolio/${name}`)))
+      portfolio: await Promise.all(getPortfolioItems().map(name => htmlPart(`portfolio/${name}`)))
     },
   }
 }
