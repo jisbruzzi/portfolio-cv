1a444e2a21921c7f155ba52a1caf55939b185f8d
jisbruzzi <jose.sbru@gmail.com>
2021-04-10 11:10:37 -0300

    unextract DarkBlock

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
 import type { GrayMatterFile } from "gray-matter"
 import React from "react"
-import DarkBlock from "../components/landing/DarkBlock"
 import Hero from "../components/landing/Hero"
 import PhotoAndInformation from "../components/landing/PhotoAndInformation"
 import { getBySlug } from "../lib/landingParts"
 import markdownToHtml from "../lib/markdownToHtml"
+
+function DarkBlock({ text }: { text: GrayMatterFile<string> }) {
+  return <div
+    className="bg-blue-900 text-white prose prose-white prose-compact max-w-none">
+    <div className="md:max-w-2xl md:mx-auto px-8 py-4">
+      {
+        text.data.title &&
+        <h1 className="text-white text-center text-6xl font-montserrat m-4 font-extrabold">
+          {text.data.title}
+        </h1>
+      }
+      <div dangerouslySetInnerHTML={{ __html: text.content }} />
+    </div>
+  </div>
+}
 
 interface HomeProps {
   jose: GrayMatterFile<string>,
