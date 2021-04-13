eef54965203d54ddfac6298ea4fcb8218d1f8594
jisbruzzi <jose.sbru@gmail.com>
2021-04-13 20:34:56 -0300

    #3 - tech section

---
+++ components/landing/Curriculum.tsx
@@ -0,0 +1,49 @@
+import { GrayMatterFile } from "gray-matter";
+import React, { useReducer } from "react";
+import Card from "./Card";
+import Hero from "./Hero";
+import PhotoAndInformation from "./PhotoAndInformation";
+import ReadMore from "./ReadMoreButton";
+
+export default function Curriculum({ main, detail }: { main: GrayMatterFile<string>, detail: GrayMatterFile<string> }) {
+  const [open, alternateOpen] = useReducer((s: boolean) => !s, false);
+  return <div className="bg-blue-50">
+    <div className="container mx-auto">
+      <div className="flex lg:flex-row flex-col align-middle">
+        <div className="
+      lg:w-1/3
+      lg:m-8
+      m-4
+      ">
+          <div className="
+        lg:min-h-almost-screen
+        sticky top-8
+        ">
+            <Card dark>
+              <PhotoAndInformation data={main.data} />
+            </Card>
+          </div>
+        </div>
+        <div className="lg:w-2/3 lg:m-8 m-4 relative">
+          <Hero data={main.data} />
+          <div
+            className="prose mx-auto my-8 prose-compact prose-weaker"
+            dangerouslySetInnerHTML={{ __html: main.content }}
+          />
+          <div className="absolute right-2">
+            <div className="relative lg:bottom-4 bottom-8">
+              <ReadMore open={open} onClick={alternateOpen} />
+            </div>
+          </div>
+          <div className={`
+          transition-height
+          overflow-hidden
+          ${open ? "" : "h-0"}
+          `}>
+            <div className="prose mx-auto my-8 prose-compact prose-weaker" dangerouslySetInnerHTML={{ __html: detail.content }} />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { GrayMatterFile } from "gray-matter"
 import React, { PropsWithChildren, useReducer } from "react"
 import Card from "../components/landing/Card"
 import ContentContainer from "../components/landing/ContentContainer"
+import Curriculum from "../components/landing/Curriculum"
 import Hero from "../components/landing/Hero"
 import PhotoAndInformation from "../components/landing/PhotoAndInformation"
 import PortfolioItem from "../components/landing/PortfolioItem"
@@ -38,56 +39,19 @@ function DarkBlock({ text }: { text: GrayMatterFile<string> }) {
   </div>
 }
 
+interface Technology{
+  name:string,
+  knowledge:number,
+}
+
 interface HomeProps {
   jose: {
     main:GrayMatterFile<string>,
     detail:GrayMatterFile<string>,
   },
   experience: GrayMatterFile<string>,
-  portfolio: GrayMatterFile<string>[]
-}
-
-function Curriculum({ main, detail }: { main: GrayMatterFile<string>,detail: GrayMatterFile<string> }) {
-  const [open, alternateOpen] = useReducer((s: boolean) => !s, false);
-  return <div className="bg-blue-50">
-    <div className="container mx-auto">
-      <div className="flex lg:flex-row flex-col align-middle">
-        <div className="
-    lg:w-1/3
-    lg:m-8
-    m-4
-    ">
-          <div className="
-      lg:min-h-almost-screen
-      sticky top-8
-      ">
-            <Card dark>
-              <PhotoAndInformation data={main.data} />
-            </Card>
-          </div>
-        </div>
-        <div className="lg:w-2/3 lg:m-8 m-4 relative">
-          <Hero data={main.data} />
-          <div
-            className="prose mx-auto my-8 prose-compact prose-weaker"
-            dangerouslySetInnerHTML={{ __html: main.content }}
-          />
-          <div className="absolute right-2">
-            <div className="relative lg:bottom-4 bottom-8">
-              <ReadMore open={open} onClick={alternateOpen} />
-            </div>
-          </div>
-          <div className={`
-        transition-height
-        overflow-hidden
-        ${open ? "" : "h-0"}
-        `}>
-            <div className="prose mx-auto my-8 prose-compact prose-weaker" dangerouslySetInnerHTML={{ __html: detail.content }} />
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  portfolio: GrayMatterFile<string>[],
+  technologies:Technology[]
 }
 
 function Portfolio(props: { portfolioItems: GrayMatterFile<string>[] }) {
@@ -98,12 +62,27 @@ function Portfolio(props: { portfolioItems: GrayMatterFile<string>[] }) {
     </ContentContainer>
   </div>
 }
+
+function Tech(props: { technologies: Technology[] }) {
+  return <div className="bg-blue-50">
+    <ContentContainer>
+      <SectionTitle> Technologies </SectionTitle>
+      <div className="flex flex-wrap justify-evenly">
+        {props.technologies.sort((a,b)=>b.knowledge-a.knowledge).map(tech => <div className="bg-white m-2 text-xl p-3 rounded-full shadow-xl">
+          {tech.name}
+        </div>)}
+      </div>
+    </ContentContainer>
+  </div>
+}
+
 export default function Homepage(props: HomeProps) {
   console.log(props)
   return <>
     <Curriculum main={props.jose.main} detail={props.jose.detail} />
     <DarkBlock text={props.experience} />
     <Portfolio portfolioItems={props.portfolio} />
+    <Tech technologies={props.technologies} />
   </>
 }
 
@@ -125,7 +104,41 @@ export async function getStaticProps(): Promise<{ props: HomeProps }> {
         detail:(await htmlPart("jose.details.md")),
       },
       experience: (await htmlPart("experience.md")),
-      portfolio: await Promise.all(getPortfolioItems().map(name => htmlPart(`portfolio/${name}`)))
+      portfolio: await Promise.all(getPortfolioItems().map(name => htmlPart(`portfolio/${name}`))),
+      technologies:[
+        { name:"python", knowledge:10 },
+        { name:"javascript", knowledge:10 },
+        { name:"react", knowledge:9 },
+        { name:"C#", knowledge:6 },
+        { name:"java", knowledge:6 },
+        { name:"SQL", knowledge:9 },
+        { name:"flask", knowledge:8 },
+        { name:"django", knowledge:6 },
+        { name:"nextjs", knowledge:8 },
+        { name:"vue", knowledge:7 },
+        { name:"ocaml", knowledge:4 },
+        { name:"lisp", knowledge:4 },
+        { name:"coq", knowledge:2 },
+        { name:"IIS", knowledge:2 },
+        { name:"terraform", knowledge:8 },
+        { name:"C", knowledge:6 },
+        { name:"C++", knowledge:4 },
+        { name:"nodeJS", knowledge:10 },
+        { name:"mongodb", knowledge:3 },
+        { name:"bash", knowledge:7 },
+        { name:"numpy", knowledge:5 },
+        { name:"bootstrap", knowledge:6 },
+        { name:"bulma", knowledge:6 },
+        { name:"webgl", knowledge:5 },
+        { name:"clojure", knowledge:4 },
+        { name:"tailwind", knowledge:7 },
+        { name:"GLSL", knowledge:2 },
+        { name:"Docker", knowledge:10 },
+        { name:"AWS", knowledge:7 },
+        { name:"Google cloud", knowledge:7 },
+        { name:"Quasar Framework", knowledge:9 },
+        { name:"Material UI", knowledge:7 },
+      ]
     },
   }
 }
